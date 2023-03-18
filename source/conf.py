06c3f76868ff8a1f51fe9f0fba157be6dee021b3
sam <sammoors@gmail.com>
2023-03-18 19:59:51 +0100

    update

--- source/conf.py
+++ source/conf.py
@@ -19,8 +19,6 @@
 
 # -- Project information -----------------------------------------------------
 
-error
-
 project = 'VSC documentation'
 copyright = '2020, VSC (Vlaams Supercomputing Center)'
 author = 'VSC (Vlaams Supercomputing Center)'
